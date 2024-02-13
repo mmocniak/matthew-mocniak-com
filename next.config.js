984ee7fa27aba40b097dd7689f1616a80ee3b1c0
Matthew Mocniak <mmocniak@users.noreply.github.com>
2024-02-13 09:56:46 -0500

    Updated IgnorePlugin syntax

--- next.config.js
+++ next.config.js
@@ -2,8 +2,13 @@ module.exports = {
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Note: we provide webpack above so you should not `require` it
     // Perform customizations to webpack config
-    config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//))
- 
+    config.plugins.push(
+      new webpack.IgnorePlugin({
+        resourceRegExp: /\/__tests__\//, // RegExp to match files that should be ignored
+      })
+    );
+    
     // Important: return the modified config
-    return config
-  }}
+    return config;
+  },
+};
