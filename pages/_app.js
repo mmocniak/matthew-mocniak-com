9871ed8a819a5b2683d247efd3fecf65b873828f
Matthew Mocniak <mmocniak@users.noreply.github.com>
2024-02-15 16:22:25 -0500

    Added Fathom Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,34 +1,39 @@
-import '../styles/globals.css'
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import * as Fathom from 'fathom-client';
+import "../styles/globals.css";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as Fathom from "fathom-client";
 
-function MyApp({ Component, pageProps }) {
-  const router = useRouter();
+function App({ Component, pageProps }) {
+    const router = useRouter();
 
-  useEffect(() => {
-    // Initialize Fathom when the app loads
-    // Example: yourdomain.com
-    //  - Do not include https://
-    //  - This must be an exact match of your domain.
-    //  - If you're using www. for your domain, make sure you include that here.
-    Fathom.load('JPSHXLBJ', {
-      includedDomains: ["www.matthewmocniak.com","matthewmocniak.com","www.mocniak.site","mocniak.site"],
-    });
+    useEffect(() => {
+        // Initialize Fathom when the app loads
+        // Example: yourdomain.com
+        //  - Do not include https://
+        //  - This must be an exact match of your domain.
+        //  - If you're using www. for your domain, make sure you include that here.
+        Fathom.load("AUCODXWK", {
+            includedDomains: [
+                "www.matthewmocniak.com",
+                "matthewmocniak.com",
+                "www.mocniak.site",
+                "mocniak.site",
+            ],
+        });
 
-    function onRouteChangeComplete() {
-      Fathom.trackPageview();
-    }
-    // Record a pageview when route changes
-    router.events.on('routeChangeComplete', onRouteChangeComplete);
+        function onRouteChangeComplete() {
+            Fathom.trackPageview();
+        }
+        // Record a pageview when route changes
+        router.events.on("routeChangeComplete", onRouteChangeComplete);
 
-    // Unassign event listener
-    return () => {
-      router.events.off('routeChangeComplete', onRouteChangeComplete);
-    };
-  }, []);
+        // Unassign event listener
+        return () => {
+            router.events.off("routeChangeComplete", onRouteChangeComplete);
+        };
+    }, []);
 
-  return <Component {...pageProps} />
+    return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default App;
